f1ee448c8baee00939898e5e8edee8c6a5cda950
pablo <pasha20030407@gmail.com>
2024-04-30 21:12:25 +0300

    fix

--- src/app/account/add-book/page.tsx
+++ src/app/account/add-book/page.tsx
@@ -1,37 +1,37 @@
-'use client'
-import React from 'react'
-import styled from '@emotion/styled'
-import { motion } from 'framer-motion'
-import Button from '@mui/material/Button'
-import CloudUploadIcon from '@mui/icons-material/CloudUpload'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select, { SelectChangeEvent } from '@mui/material/Select'
-import TextField from '@mui/material/TextField'
+"use client";
+import React from "react";
+import styled from "@emotion/styled";
+import { motion } from "framer-motion";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
 
 const bookAuthors = [
-  { value: 'Agatha Christie', label: 'Agatha Christie' },
-  { value: 'J.K. Rowling', label: 'J.K. Rowling' },
-  { value: 'George Orwell', label: 'George Orwell' },
-  { value: 'Jane Austen', label: 'Jane Austen' },
-  { value: 'Stephen King', label: 'Stephen King' },
-  { value: 'Harper Lee', label: 'Harper Lee' },
-  { value: 'J.R.R. Tolkien', label: 'J.R.R. Tolkien' },
-  { value: 'F. Scott Fitzgerald', label: 'F. Scott Fitzgerald' },
-  { value: 'Mark Twain', label: 'Mark Twain' },
-  { value: 'Ernest Hemingway', label: 'Ernest Hemingway' },
-  { value: 'Charles Dickens', label: 'Charles Dickens' },
-  { value: 'Gabriel García Márquez', label: 'Gabriel García Márquez' },
-  { value: 'Leo Tolstoy', label: 'Leo Tolstoy' },
-  { value: 'Emily Brontë', label: 'Emily Brontë' },
-  { value: 'Herman Melville', label: 'Herman Melville' },
-  { value: 'Virginia Woolf', label: 'Virginia Woolf' },
-  { value: 'Aldous Huxley', label: 'Aldous Huxley' },
-  { value: 'Homer', label: 'Homer' },
-  { value: 'Toni Morrison', label: 'Toni Morrison' },
-  { value: 'Kazuo Ishiguro', label: 'Kazuo Ishiguro' },
-]
+  { value: "Agatha Christie", label: "Agatha Christie" },
+  { value: "J.K. Rowling", label: "J.K. Rowling" },
+  { value: "George Orwell", label: "George Orwell" },
+  { value: "Jane Austen", label: "Jane Austen" },
+  { value: "Stephen King", label: "Stephen King" },
+  { value: "Harper Lee", label: "Harper Lee" },
+  { value: "J.R.R. Tolkien", label: "J.R.R. Tolkien" },
+  { value: "F. Scott Fitzgerald", label: "F. Scott Fitzgerald" },
+  { value: "Mark Twain", label: "Mark Twain" },
+  { value: "Ernest Hemingway", label: "Ernest Hemingway" },
+  { value: "Charles Dickens", label: "Charles Dickens" },
+  { value: "Gabriel García Márquez", label: "Gabriel García Márquez" },
+  { value: "Leo Tolstoy", label: "Leo Tolstoy" },
+  { value: "Emily Brontë", label: "Emily Brontë" },
+  { value: "Herman Melville", label: "Herman Melville" },
+  { value: "Virginia Woolf", label: "Virginia Woolf" },
+  { value: "Aldous Huxley", label: "Aldous Huxley" },
+  { value: "Homer", label: "Homer" },
+  { value: "Toni Morrison", label: "Toni Morrison" },
+  { value: "Kazuo Ishiguro", label: "Kazuo Ishiguro" },
+];
 
 const AccountInner = styled.div`
   display: flex;
@@ -40,26 +40,28 @@ const AccountInner = styled.div`
   justify-content: center;
   gap: 20px;
   max-width: 500px;
-`
+`;
 
 const Title = styled.h1`
   margin-top: 30px;
   font-size: 38px;
   color: #0a0a0a;
-`
+`;
 
 const ButtonAuth = styled.button<{ isOutlined: boolean }>`
   ${(p) => `
   padding: 10px 20px;
-  background-color: ${p.isOutlined ? 'transparent' : 'rgba(255, 255, 255, 0.9)'};
+  background-color: ${
+    p.isOutlined ? "transparent" : "rgba(255, 255, 255, 0.9)"
+  };
   border: 2px solid #0a0a0a;
-  color: ${p.isOutlined ? 'rgba(255, 255, 255, 0.9)' : '#0a0a0a'};
+  color: ${p.isOutlined ? "rgba(255, 255, 255, 0.9)" : "#0a0a0a"};
   font-weight: 600;
   font-size: 16px;
   cursor: pointer;
   border-radius: 4px;
   `}
-`
+`;
 const rightAnimation = {
   hidden: { x: 100, opacity: 0, transition: { duration: 0.5 } },
   visible: (custom: number) => ({
@@ -67,70 +69,22 @@ const rightAnimation = {
     opacity: 1,
     transition: { delay: custom * 0.2, duration: 0.3 },
   }),
-}
+};
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
   height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
+  overflow: "hidden",
+  position: "absolute",
   bottom: 0,
   left: 0,
-  whiteSpace: 'nowrap',
+  whiteSpace: "nowrap",
   width: 1,
-})
+});
 
 const Account = () => {
-  const [addAuthor, setAddAuthor] = React.useState<'manual' | 'select'>('manual')
-
-  const [author, setAuthor] = React.useState('' as string)
-
-  const handleAuthorChange = (event: SelectChangeEvent<string>) => {
-    setAuthor(event.target.value as string)
-  }
-
-  const handleChange = (event: SelectChangeEvent<string>) => {
-    setAddAuthor(event.target.value as 'manual' | 'select')
-  }
-
-  const authorInputs = {
-    manual: (
-      <TextField
-        sx={{
-          outline: 'none',
-          '& .MuiOutlinedInput-notchedOutline': {
-            border: '2px solid #171717',
-          },
-        }}
-        type="text"
-        label="Введіть автора"
-        fullWidth
-      />
-    ),
-    select: (
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Виберіть автора</InputLabel>
-        <Select
-          sx={{
-            outline: 'none',
-            '& .MuiOutlinedInput-notchedOutline': {
-              border: '2px solid #171717',
-            },
-          }}
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={author}
-          label="Виберіть автора"
-          onChange={handleAuthorChange}
-        >
-          {bookAuthors.map((author) => (
-            <MenuItem value={author.value}>{author.label}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    ),
-  }
+  const [genre, setGenre] = React.useState("");
 
   return (
     <>
@@ -143,40 +97,55 @@ const Account = () => {
           <motion.div
             custom={1}
             variants={rightAnimation}
-            style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
+            style={{ width: "100%", display: "flex", justifyContent: "center" }}
           >
             <Title>Add Book</Title>
           </motion.div>
-          <motion.div custom={2} variants={rightAnimation} style={{ width: '100%' }}>
+
+          <motion.div
+            custom={2}
+            variants={rightAnimation}
+            style={{ width: "100%" }}
+          >
             <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Автор</InputLabel>
+              <InputLabel htmlFor="my-input">Автор</InputLabel>
               <Select
                 sx={{
-                  outline: 'none',
-                  '& .MuiOutlinedInput-notchedOutline': {
-                    border: '2px solid #171717',
+                  outline: "none",
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    border: "2px solid #171717",
                   },
                 }}
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={addAuthor}
                 label="Автор"
-                onChange={handleChange}
+                labelId="demo-simple-select-label"
+                value={genre}
+                onChange={(e: SelectChangeEvent) => setGenre(e.target.value)}
+                fullWidth
               >
-                <MenuItem value={'manual'}>Додати в ручну</MenuItem>
-                <MenuItem value={'select'}>Вибрати автора</MenuItem>
+                <MenuItem value={"1"}>Фентезі</MenuItem>
+                <MenuItem value={"2"}>Детектив</MenuItem>
+                <MenuItem value={"3"}>Драма</MenuItem>
+                <MenuItem value={"4"}>Історичний роман</MenuItem>
+                <MenuItem value={"5"}>Класика</MenuItem>
+                <MenuItem value={"6"}>Фантастика</MenuItem>
+                <MenuItem value={"7"}>Поезія</MenuItem>
+                <MenuItem value={"8"}>Пригоди</MenuItem>
+                <MenuItem value={"9"}>Роман</MenuItem>
+                <MenuItem value={"10"}>Трилер</MenuItem>
               </Select>
             </FormControl>
           </motion.div>
-          <motion.div custom={3} variants={rightAnimation} style={{ width: '100%' }}>
-            {authorInputs[addAuthor]}
-          </motion.div>
-          <motion.div custom={4} variants={rightAnimation} style={{ width: '100%' }}>
+
+          <motion.div
+            custom={4}
+            variants={rightAnimation}
+            style={{ width: "100%" }}
+          >
             <TextField
               sx={{
-                outline: 'none',
-                '& .MuiOutlinedInput-notchedOutline': {
-                  border: '2px solid #171717',
+                outline: "none",
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: "2px solid #171717",
                 },
               }}
               type="text"
@@ -184,12 +153,16 @@ const Account = () => {
               fullWidth
             />
           </motion.div>
-          <motion.div custom={5} variants={rightAnimation} style={{ width: '100%' }}>
+          <motion.div
+            custom={5}
+            variants={rightAnimation}
+            style={{ width: "100%" }}
+          >
             <TextField
               sx={{
-                outline: 'none',
-                '& .MuiOutlinedInput-notchedOutline': {
-                  border: '2px solid #171717',
+                outline: "none",
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: "2px solid #171717",
                 },
               }}
               type="text"
@@ -199,12 +172,16 @@ const Account = () => {
               rows={4}
             />
           </motion.div>
-          <motion.div custom={6} variants={rightAnimation} style={{ width: '100%' }}>
+          <motion.div
+            custom={6}
+            variants={rightAnimation}
+            style={{ width: "100%" }}
+          >
             <Button
               sx={{
-                fontFamily: 'Inter, sans-serif',
-                fontSize: '16px',
-                fontWeight: '500',
+                fontFamily: "Inter, sans-serif",
+                fontSize: "16px",
+                fontWeight: "500",
               }}
               component="label"
               variant="contained"
@@ -219,19 +196,19 @@ const Account = () => {
           <motion.div
             custom={7}
             variants={rightAnimation}
-            style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
+            style={{ width: "100%", display: "flex", justifyContent: "center" }}
           >
             <Button
               sx={{
-                marginBottom: '30px',
-                fontFamily: 'Inter, sans-serif',
-                fontSize: '16px',
-                fontWeight: '500',
-                backgroundColor: '#171717',
-                color: 'rgba(255, 255, 255, 0.9)',
-                '&:hover': {
-                  backgroundColor: '#171717',
-                  color: 'rgba(255, 255, 255, 0.9)',
+                marginBottom: "30px",
+                fontFamily: "Inter, sans-serif",
+                fontSize: "16px",
+                fontWeight: "500",
+                backgroundColor: "#171717",
+                color: "rgba(255, 255, 255, 0.9)",
+                "&:hover": {
+                  backgroundColor: "#171717",
+                  color: "rgba(255, 255, 255, 0.9)",
                 },
               }}
               variant="contained"
@@ -243,7 +220,7 @@ const Account = () => {
         </AccountInner>
       </motion.div>
     </>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,7 +1,7 @@
-'use client'
-import React from 'react'
-import styled from '@emotion/styled'
-import { motion } from 'framer-motion'
+"use client";
+import React from "react";
+import styled from "@emotion/styled";
+import { motion } from "framer-motion";
 
 const Container = styled.div`
   display: flex;
@@ -9,7 +9,7 @@ const Container = styled.div`
   align-items: center;
   height: calc(100vh - 64px);
   background-color: #0a0a0a;
-`
+`;
 
 const Input = styled.input`
   margin-bottom: 20px;
@@ -20,32 +20,34 @@ const Input = styled.input`
   outline: none;
   background-color: #171717;
   color: rgba(255, 255, 255, 0.9);
-`
+`;
 
 const ButtonAuth = styled.button<{ isOutlined: boolean }>`
   ${(p) => `
   padding: 10px 20px;
-  background-color: ${p.isOutlined ? 'transparent' : 'rgba(255, 255, 255, 0.9)'};
+  background-color: ${
+    p.isOutlined ? "transparent" : "rgba(255, 255, 255, 0.9)"
+  };
   border: 1px solid rgba(255, 255, 255, 0.6);
-  color: ${p.isOutlined ? 'rgba(255, 255, 255, 0.9)' : '#0a0a0a'};
+  color: ${p.isOutlined ? "rgba(255, 255, 255, 0.9)" : "#0a0a0a"};
   border-radius: 6px;
   font-weight: 600;
   font-size: 16px;
   cursor: pointer;
   `}
-`
+`;
 
 const Title = styled.h1`
   margin-bottom: 30px;
   font-size: 38px;
   color: rgba(255, 255, 255, 0.9);
-`
+`;
 const AuthBlock = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding-top: calc(100vh * 0.2);
-`
+`;
 
 const animation = {
   hidden: { y: -100, opacity: 0 },
@@ -54,7 +56,7 @@ const animation = {
     opacity: 1,
     transition: { delay: custom * 0.2 },
   }),
-}
+};
 
 const SignIn = () => {
   return (
@@ -65,21 +67,27 @@ const SignIn = () => {
             <Title>Sign in to BookHub</Title>
           </motion.div>
           <motion.div custom={2} variants={animation}>
-            <Input type="email" placeholder="Email" />
+            <Input type="text" placeholder="Name" />
           </motion.div>
           <motion.div custom={3} variants={animation}>
-            <Input type="password" placeholder="Password" />
+            <Input type="text" placeholder="Surname" />
           </motion.div>
           <motion.div custom={4} variants={animation}>
-            <Input type="password" placeholder="Repeat password" />
+            <Input type="email" placeholder="Email" />
           </motion.div>
           <motion.div custom={5} variants={animation}>
+            <Input type="password" placeholder="Password" />
+          </motion.div>
+          <motion.div custom={6} variants={animation}>
+            <Input type="password" placeholder="Repeat password" />
+          </motion.div>
+          <motion.div custom={7} variants={animation}>
             <ButtonAuth isOutlined>Create account</ButtonAuth>
           </motion.div>
         </AuthBlock>
       </Container>
     </motion.div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
